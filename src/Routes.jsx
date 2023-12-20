f2936e6dca915e286fa0424b80c8a0e82dfbdde5
Larebb <larebarain9@gmail.com>
2023-12-20 16:32:14 +0500

    first commit

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
 const LandingpageOne = React.lazy(() => import("pages/LandingpageOne"));
+
 const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Navigate to="/landingpageone" replace />
+                <Home />
+              </>
+            }
+          />
           <Route path="/landingpageone" element={<LandingpageOne />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </React.Suspense>
   );
 };
+
 export default ProjectRoutes;
